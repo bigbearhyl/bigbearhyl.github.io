16c23fbdb64a22b01796ba6a38e1e9a6effbf334
zekahyl <3341472580@qq.com>
2023-08-27 20:16:35 +0800

    Site updated: 2023-08-27 20:16:34

---
+++ js/weather.js
@@ -0,0 +1,24 @@
+WIDGET = {
+  "CONFIG": {
+    "modules": "01234",
+    "background": "1",
+    "tmpColor": "B6D7A8",
+    "tmpSize": "24",
+    "cityColor": "FFFFFF",
+    "citySize": "0",
+    "aqiColor": "FFF2CC",
+    "aqiSize": "24",
+    "weatherIconSize": "24",
+    "alertIconSize": "24",
+    "padding": "px 10px 10px 10px",
+    "shadow": "1",
+    "language": "zh",
+    "borderRadius": "8",
+    "fixed": "true",
+    "vertical": "left",
+    "horizontal": "right",
+    "left": "200",
+    "top": "15",
+    "key": "7c4f530bfbb64e52a7c2463b1cc024a6"
+  }
+}
